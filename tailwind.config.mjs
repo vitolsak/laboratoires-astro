ad8655f63aee01cdf9456af3d3b8c28c2c360127
Vit Olsak <olsi@Mac.lan>
2025-07-11 09:12:07 +0200

    fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,10 +3,16 @@ export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         extend: {
-            fontFamily: { 
-                sans: ['Inter', 'sans-serif'], 
-                serif: ['Playfair Display', 'serif'], 
-            },
+            fontFamily: {
+  sans: [
+    'Inter',
+    {
+      fontFeatureSettings: '"cv05", "cv06", "cv08"', // Optional: For nicer looking numbers and characters
+    },
+    'sans-serif',
+  ],
+  serif: ['Playfair Display', 'serif'],
+},
             colors: { 
                 'brand-primary': '#2d332a', 
                 'brand-secondary': '#f8f7f5', 
